e2bffea3603dc546d48664e7b0af23abc147eab4
Ismail020 <isocetin1907@gmail.com>
2023-10-10 11:34:52 +0200

    Computing usernames

--- script.js
+++ script.js
@@ -81,3 +81,16 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsernames = function (acc) {
+  acc.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
